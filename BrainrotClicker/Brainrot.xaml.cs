8a2368760c51734a91ab3e11e9c6f3488f837b4d
4313657 <4313657@TE-GDA-D-D37H10>
2025-05-19 13:19:58 +0200

    asdfcvasfc

--- BrainrotClicker/Brainrot.xaml.cs
+++ BrainrotClicker/Brainrot.xaml.cs
@@ -1,27 +1,65 @@
-
 using Plugin.Maui.Audio;
 
 namespace BrainrotClicker;
 
 public partial class Brainrot : ContentPage
 {
-    private readonly IAudioPlayer _player;
+    private IAudioPlayer _player;
+
     int points = 0;
     int upgValue = 1;
+    public string songPath = "braun.mp3";
+
+    private string[] imagePaths = new string[]
+    {
+        "tralalero.jpg", 
+        "balerina.jpg", 
+        "patapim.jpg", 
+        "bombardilo.jpg", 
+        "trippu.jpg", 
+        "tung.jpg"  
+    };
+    private string[] songPaths = new string[]
+    {
+        "tralalero.mp3",
+        "balerinacapucina.mp3",
+        "brrbrrpatapim.mp3",
+        "bombardilocrocodilo.mp3",
+        "trippitroppa.mp3",
+        "tungsahur.mp3"
+    };
+
+    private int currentStateIndex = 0; // Indeks aktualnego stanu obrazka
+
     public Brainrot()
-	{
-		InitializeComponent();
-           var audioManager = AudioManager.Current;
-        var stream = FileSystem.OpenAppPackageFileAsync("braun.mp3").Result;
-        _player = audioManager.CreatePlayer(stream);
-	}
+    {
+        InitializeComponent();
+        _ = LoadSong(); // uruchom asynchronicznie
+        UpdateButtonState(); // poka¿ pierwszy obrazek
+    }
+    private async Task LoadSong()
+    {
+        try
+        {
+            var audioManager = AudioManager.Current;
+            var stream = await FileSystem.OpenAppPackageFileAsync(songPath);
+
+            _player?.Stop();
+            _player?.Dispose();
+            _player = audioManager.CreatePlayer(stream);
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("B³¹d", $"Nie uda³o siê za³adowaæ piosenki: {ex.Message}", "OK");
+        }
+    }
 
 
-    private void UpgClick(object sender, EventArgs e)
+    private void Klik(object sender, EventArgs e)
     {
         _player.Play();
         brainrotClick();
-        
+
     }
     public void brainrotClick()
     {
@@ -30,12 +68,13 @@ public partial class Brainrot : ContentPage
     }
     private void Upd1(object sender, EventArgs e)
     {
-        if(points > 10) { 
+        if (points > 10)
+        {
             upgValue += 1;
             points -= 10;
             brainrotClickValue.Text = upgValue.ToString();
         }
-        
+
     }
     private void Upd2(object sender, EventArgs e)
     {
@@ -60,8 +99,27 @@ public partial class Brainrot : ContentPage
         if (points > 1000)
         {
             upgValue += 1;
-            points -= 10;
+            points -= 10000;
             brainrotClickValue.Text = upgValue.ToString();
         }
     }
+    private async void UpdateButtonState()
+    {
+        KlickImage.Source = imagePaths[currentStateIndex];
+        songPath = songPaths[currentStateIndex];
+        await LoadSong(); // teraz mo¿esz poczekaæ na za³adowanie
+    }
+
+    private void OnGoraClicked(object sender, EventArgs e)
+    {
+        currentStateIndex = (currentStateIndex + 1) % imagePaths.Length;
+        UpdateButtonState();
+    }
+
+    private void OnDolClicked(object sender, EventArgs e)
+    {
+        currentStateIndex = (currentStateIndex - 1 + imagePaths.Length) % imagePaths.Length; 
+        UpdateButtonState();
+    }
 }
+
